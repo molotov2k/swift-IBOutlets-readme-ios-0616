92170fb71ea210224bdbaef12741895831064513
molotov2k <molotov2k@gmail.com>
2016-07-08 12:10:39 -0400

    done

--- IBOutletFun/IBOutletFun/ViewController.swift
+++ IBOutletFun/IBOutletFun/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var labelOutlet: UILabel!
+    @IBOutlet weak var helloWorldOutlet: UIButton!
+    @IBOutlet weak var goodbuyWorldOutlet: UIButton!
+    @IBOutlet weak var unicornOutlet: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,4 +23,20 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func helloWorldAction(sender: AnyObject) {
+        labelOutlet.text = helloWorldOutlet.titleLabel?.text
+    }
+    
+    @IBAction func goodbuyWorldAction(sender: AnyObject) {
+        labelOutlet.text = goodbuyWorldOutlet.titleLabel?.text
+    }
+    
+    @IBAction func unicornAction(sender: AnyObject) {
+        labelOutlet.text = unicornOutlet.titleLabel?.text
+    }
+    
+    
+    
+    
 }
